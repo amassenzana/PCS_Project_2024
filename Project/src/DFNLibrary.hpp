87af87a6155362d3f8691091a6d8946b40bba28d
Monado777 <164095745+Monado777@users.noreply.github.com>
2024-06-02 16:47:20 +0200

    Update DFNLibrary.hpp

--- Project/src/DFNLibrary.hpp
+++ Project/src/DFNLibrary.hpp
@@ -3,10 +3,13 @@
 #include "Eigen/Eigen"
 #include <iostream>
 #include <vector>
+#include <variant>
 
 
 namespace DFNLibrary{
-
+struct Point {
+    double x, y;
+};
 struct Frattura{
     int id;
     int numVert;
@@ -18,6 +21,9 @@ struct Frattura{
     void computeCenter();
     void computeRadius();
     void computePlane();
+    std::vector<Eigen::Vector3d> IntersectionPoints;
+    std::vector<Eigen::Vector2d> GoodValsDouble;
+    std::vector<std::vector<Eigen::Vector3d>> GoodValsVec; // Tensore
 };
 
 struct Traccia{
@@ -36,6 +42,13 @@ struct DFN{
 bool importDFN(std::string path, DFN& dfn);
 bool checkIntersection(Frattura& f1, Frattura& f2, std::vector<Eigen::Vector3d>& v);
 int sign(double d);
+Eigen::Vector2d ProjXY( Eigen::Vector3d tmpvec_xy);
+Eigen::Vector2d ProjXZ( Eigen::Vector3d tmpvec_xz);
+Eigen::Vector2d ProjYZ( Eigen::Vector3d tmpvec_yz);
+std::pair<Eigen::Vector2d, bool> ProjIntersection(Eigen::Vector2d a1, Eigen::Vector2d a2, Eigen::Vector2d b1, Eigen::Vector2d b2);
+Eigen::Vector3d IntersectVerif(Eigen::Vector3d p1, Eigen::Vector3d d1, Eigen::Vector3d p2, Eigen::Vector3d d2);
+bool areParallel(DFNLibrary::Point line1_p1, DFNLibrary::Point line1_p2, DFNLibrary::Point line2_p1, DFNLibrary::Point line2_p2);
+Eigen::Vector3d IntersecPointCalc(int i, int j);
 } // namespace DFNLibrary
 
 
