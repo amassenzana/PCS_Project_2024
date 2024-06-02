165f16f11ae1c22280b0c9febf8d73e41a19584e
Monado777 <164095745+Monado777@users.noreply.github.com>
2024-06-02 16:46:35 +0200

    Update DFNLibrary.cpp

--- Project/src/DFNLibrary.cpp
+++ Project/src/DFNLibrary.cpp
@@ -59,7 +59,7 @@ void DFNLibrary::Frattura::computeCenter(){
 
     for(int i = 1; i < numVert+1; i++){
         double w =  (vertices[i%numVert]-vertices[(i+1)%numVert]).norm() +
-                    (vertices[i%numVert]-vertices[(i-1)%numVert]).norm();
+                   (vertices[i%numVert]-vertices[(i-1)%numVert]).norm();
         c += vertices[i%numVert]*w;
         sw += w;
     }
@@ -104,12 +104,82 @@ void DFNLibrary::DFN::computeDFN(){
             vector<Eigen::Vector3d> v;
             if(i!=j && checkIntersection(fractures[i], fractures[j], v)){
                 cout << "Intersezione tra f[" << i << "] e f[" << j << "]:" << endl;
+                for (int k = 0; k < fractures[i].IntersectionPoints.size(); k++)
+                {
+                    cout << fractures[i].IntersectionPoints[k] << endl;
+                }
 
             }
         }
     }
 
 
+}
+
+// 1)   Funzioni di proiezione
+Eigen::Vector2d DFNLibrary::ProjXY(Eigen::Vector3d tmpvec_xy)
+{
+    Eigen::Vector2d projvec_xy (tmpvec_xy[0], tmpvec_xy[1]);
+    return projvec_xy;
+}
+
+Eigen::Vector2d DFNLibrary::ProjXZ(Eigen::Vector3d tmpvec_xz)
+{
+    Eigen::Vector2d projvec_xz (tmpvec_xz[0], tmpvec_xz[2]);
+    return projvec_xz;
+}
+
+Eigen::Vector2d DFNLibrary::ProjYZ(Eigen::Vector3d tmpvec_yz)
+{
+    Eigen::Vector2d projvec_yz (tmpvec_yz[1], tmpvec_yz[2]);
+    return projvec_yz;
+}
+
+
+// 2)   Ricerca del punto di intersezione nelle proiezioni
+
+std::pair<Eigen::Vector2d, bool> DFNLibrary::ProjIntersection(Eigen::Vector2d a1, Eigen::Vector2d a2, Eigen::Vector2d b1, Eigen::Vector2d b2)
+{
+    using Line2 = Eigen::Hyperplane<double,2>;
+    using Vec2 = Eigen::Vector2d;
+    bool tmpcheck = false;
+
+    Vec2 a(a1[0], a1[1]);
+    Vec2 b(a2[0], a2[1]);
+
+    Vec2 c(b1[0], b1[1]);
+    Vec2 d(b2[0], b2[1]);
+
+    Line2 ab = Line2::Through(a,b);
+    Line2 cd = Line2::Through(c,d);
+
+    Vec2 qp = ab.intersection(cd);
+
+    // Escludo le linee coincidenti
+    if (!ab.isApprox(cd))
+    {
+        // Escludo le linee parallele
+        if (qp[0] != std::numeric_limits<double>::infinity() || qp[1] != std::numeric_limits<double>::infinity())
+        {
+            tmpcheck = true;
+        }
+    }
+
+    return std::make_pair(qp,tmpcheck);
+};
+
+// qp è il punto di partenza della retta d'intersezione tra i due piani generati dalle fratture in uso.
+
+// 3)   Funzione che calcola l'intersezione tra due linee in uno spazio 3D.
+// Adattare l'algoritmo per ogni proiezione possibile dai 3 piani.
+// qp è il punto ottenuto dalla proiezione, dqp e il vettore direzione della proiezione del punto in una linea infinita, mentre p1 e p2 sono due punti della retta con la
+//quale si effettua il confronto.
+Eigen::Vector3d DFNLibrary::IntersectVerif(Eigen::Vector3d qp_l, Eigen::Vector3d dqp, Eigen::Vector3d p1, Eigen::Vector3d p2) {
+    Eigen::Vector3d d1 = p2-p1;
+    Eigen::Vector3d n = dqp.cross(d1);
+    Eigen::Vector3d sr = qp_l - p1;
+    double t = (sr.cross(d1)).dot(n) / n.dot(n);
+    return qp_l + dqp * t;
 }
 
 bool DFNLibrary::checkIntersection(Frattura &f1, Frattura &f2, vector<Eigen::Vector3d> &v){
@@ -142,80 +212,108 @@ bool DFNLibrary::checkIntersection(Frattura &f1, Frattura &f2, vector<Eigen::Vec
     if(!flag) return false;
 
 
-    Eigen::Matrix2d MAT;
-    Eigen::Vector2d TN;
+    // INTERSEZIONE
+    Eigen::Vector3d tmpvec1_1 , tmpvec1_2;
+    Eigen::Vector3d tmpvec2_1 , tmpvec2_2;
+    Eigen::Vector2d prj1_1_xy , prj1_1_xz, prj1_1_yz, prj1_2_xy , prj1_2_xz, prj1_2_yz;
+    Eigen::Vector2d prj2_1_xy , prj2_1_xz, prj2_1_yz, prj2_2_xy , prj2_2_xz, prj2_2_yz;
+    int tmpcount = 0;
+    for (int i = 0; i <= f1.numVert; i++)
+    {
+        Eigen::Vector3d tmpvec1_1 = f1.vertices[i%f1.numVert];
+        Eigen::Vector3d tmpvec1_2 = f1.vertices[(i+1)%f1.numVert];
+        Eigen::Vector2d prj1_1_xy = ProjXY(tmpvec1_1);
+        Eigen::Vector2d prj1_1_xz = ProjXZ(tmpvec1_1);
+        Eigen::Vector2d prj1_1_yz = ProjYZ(tmpvec1_1);
+        Eigen::Vector2d prj1_2_xy = ProjXY(tmpvec1_2);
+        Eigen::Vector2d prj1_2_xz = ProjXZ(tmpvec1_2);
+        Eigen::Vector2d prj1_2_yz = ProjYZ(tmpvec1_2);
+        for (int j = 0; j < f2.numVert; j++)
+        {
+            tmpvec2_1 = f2.vertices[j%f2.numVert];
+            tmpvec2_2 = f2.vertices[(j+1)%f2.numVert];
+            prj2_1_xy = ProjXY(tmpvec2_1);
+            prj2_1_xz = ProjXZ(tmpvec2_1);
+            prj2_1_yz = ProjYZ(tmpvec2_1);
+            prj2_2_xy = ProjXY(tmpvec2_2);
+            prj2_2_xz = ProjXZ(tmpvec2_2);
+            prj2_2_yz = ProjYZ(tmpvec2_2);
 
-    MAT << f1.planeC[0], f1.planeC[1],
-         f2.planeC[0], f2.planeC[1];
-    TN << -f1.planeD, -f2.planeD;
-
-    Eigen::Vector2d x = MAT.colPivHouseholderQr().solve(TN);
-    Eigen::Vector3d p1{{x(0),x(1),0}};
-    // Eigen::Vector3d P{{0.017584598939879986, 0, 0}};
-    Eigen::Vector3d v1 = f1.planeC.cross(f2.planeC);
-
-
-    // vector<Eigen::Vector3d> v;
-    v.reserve(4);
-
-    for(int i = 0; i < f1.numVert; i++){
-        Eigen::Vector3d B = f1.vertices[i], p2 = f1.vertices[(i+1)%f1.numVert];
-        Eigen::Vector3d v2 = B-p2;
-
-        Eigen::Vector3d w0 = p1-p2;
-        double a = v1.dot(v1);
-        double b = v1.dot(v2);
-        double c = v2.dot(v2);
-        double d = v1.dot(w0);
-        double e = v2.dot(w0);
-
-        double denom = a*c - b*b;
-        if (denom == 0) {
-            // std::cout << "Le rette sono parallele o coincidenti.\n";
-            continue;
+            auto result = ProjIntersection(prj1_1_xy, prj1_2_xy, prj2_1_xy, prj2_2_xy);
+            bool tmpflag = result.second;
+            if (tmpflag == true)
+            {
+                Eigen::Vector2d qp = result.first;
+                Eigen::Vector3d qp_xy (qp[0], qp[1], 0);
+                Eigen::Vector3d dqp (0, 0, 1);
+                // f1.IntersectionPoints.push_back(IntersectVerif(qp_xy, dqp, tmpvec2_1, tmpvec2_2));
+                tmpcount++;
+                f1.GoodValsDouble.push_back(Eigen::Vector2d (i,j));
+                std::vector<Eigen::Vector3d> tmpvec = {qp_xy, dqp, tmpvec2_1, tmpvec2_2};
+                f1.GoodValsVec.push_back(tmpvec);
+            }
+            else
+            {
+                auto result = ProjIntersection(prj1_1_xz, prj1_2_xz, prj2_1_xz, prj2_2_xz);
+                bool tmpflag = result.second;
+                if (tmpflag == true)
+                {
+                    Eigen::Vector2d qp = result.first;
+                    Eigen::Vector3d qp_xz (qp[0], 0, qp[1]);
+                    Eigen::Vector3d dqp (0, 1, 0);
+                    // f1.IntersectionPoints.push_back(IntersectVerif(qp_xz, dqp, tmpvec2_1, tmpvec2_2));
+                    tmpcount++;
+                    f1.GoodValsDouble.push_back(Eigen::Vector2d (i,j));
+                    std::vector<Eigen::Vector3d> tmpvec = {qp_xz, dqp, tmpvec2_1, tmpvec2_2};
+                    f1.GoodValsVec.push_back(tmpvec);
+                }
+                else
+                {
+                    auto result = ProjIntersection(prj1_1_yz, prj1_2_yz, prj2_1_yz, prj2_2_yz);
+                    bool tmpflag = result.second;
+                    if (tmpflag == true)
+                    {
+                        Eigen::Vector2d qp = result.first;
+                        Eigen::Vector3d qp_yz (0, qp[0], qp[1]);
+                        Eigen::Vector3d dqp (1, 0, 0);
+                        // f1.IntersectionPoints.push_back(IntersectVerif(qp_yz, dqp, tmpvec2_1, tmpvec2_2));
+                        tmpcount++;
+                        f1.GoodValsDouble.push_back(Eigen::Vector2d (i,j));
+                        std::vector<Eigen::Vector3d> tmpvec = {qp_yz, dqp, tmpvec2_1, tmpvec2_2};
+                        f1.GoodValsVec.push_back(tmpvec);
+                    }
+                }
+            }
         }
-        double sc = (b*e - c*d) / denom;
-        double tc = (a*e - b*d) / denom;
-
-        Eigen::Vector3d intersection = p2 + tc*v2;
-        if(tc > -tol && tc < 1+tol)
-            v.push_back(intersection);
     }
 
-
-    for(int i = 0; i < f2.numVert; i++){
-        Eigen::Vector3d B = f2.vertices[i], p2 = f2.vertices[(i+1)%f2.numVert];
-        Eigen::Vector3d v2 = B-p2;
-
-        Eigen::Vector3d w0 = p1-p2;
-        double a = v1.dot(v1);
-        double b = v1.dot(v2);
-        double c = v2.dot(v2);
-        double d = v1.dot(w0);
-        double e = v2.dot(w0);
-
-        double denom = a*c - b*b;
-        if (denom == 0) {
-            // std::cout << "Le rette sono parallele o coincidenti.\n";
-            continue;
+    for (int m = 0; m < f1.GoodValsDouble.size(); m++)
+    {
+        for (int n = 0; n < f2.GoodValsDouble.size(); n++)
+        {
+            std::vector j1 = {f1.GoodValsDouble[m][0], f1.GoodValsDouble[m][0]+1};
+            std::vector j2 = {f1.GoodValsDouble[m][1], f1.GoodValsDouble[m][0]+1};
+            tmpvec2_1 = f2.vertices[int (j1[0])%f2.numVert];
+            tmpvec2_2 = f2.vertices[int (j1[1])%f2.numVert];
+            // Eigen::Vector3d tmpvecfinal = IntersectVerif(f1.GoodValsVec[m][0], f1.GoodValsVec[m][1], f1.GoodValsVec[m][2], f1.GoodValsVec[m][3]);
+            Eigen::Vector3d tmpvecfinal = IntersectVerif(f1.GoodValsVec[m][0], f1.GoodValsVec[m][1], tmpvec2_1, tmpvec2_2);
+            f1.IntersectionPoints.push_back(tmpvecfinal);
         }
-        double sc = (b*e - c*d) / denom;
-        double tc = (a*e - b*d) / denom;
-
-        Eigen::Vector3d intersection = p2 + tc*v2;
-        if(tc > -tol && tc < 1+tol)
-            v.push_back(intersection);
-
     }
 
-    if(v.size()>0)
+    if (tmpcount > 0)
+    {
         return true;
-
-
-
-
-    return false;
+    }
+    else
+    {
+        return false;
+    }
+    // tmpcount = 0;
 }
+
+// Eigen::Vector3d IntersecPointCalc
+
 
 int DFNLibrary::sign(double d){
     if (d>tol) return 1;
